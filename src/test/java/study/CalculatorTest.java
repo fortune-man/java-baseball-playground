5cbc4eec6e760a2e18b0f5ac9f849e1f227e95b4
fortune-man <wnguddl96@knou.ac.kr>
2024-06-19 11:56:48 +0900

    test(CalculatorTest) : 계산기 테스트 코드 추가

---
+++ src/test/java/study/CalculatorTest.java
@@ -0,0 +1,38 @@
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * 요구사항
+ * 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기를 구현해야 한다.
+ * 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다. 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다.
+ * 예를 들어 "2 + 3 * 4 / 2"와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력해야 한다.
+ */
+public class CalculatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2", "3", "4"})
+    void 문자열_입력_테스트(String request) {
+        // given
+        int[] expect = {1, 2, 3, 4};
+
+        int actual = Integer.parseInt(request);
+
+        boolean assertion = Arrays.stream(expect).anyMatch(i -> i == actual);
+        assertTrue(assertion);
+    }
+
+}
